ca433dfbaeda9c5814d3b3b182a4e029e92c8c64
yulu9206@gmail.com <yulu9206@Lus-MacBook-Air.local>
2017-09-17 16:59:21 -0700

    loginReg

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 // var passport = require('passport');
 // var LocalStrategy = require('passport-local').Strategy;
 
-// var User = require('../models/user');
+var User = require('../models/user');
 
 // Register
 router.get('/register', function(req, res){
@@ -16,45 +16,45 @@ router.get('/login', function(req, res){
 });
 
 // Register User
-// router.post('/register', function(req, res){
-// 	var name = req.body.name;
-// 	var email = req.body.email;
-// 	var username = req.body.username;
-// 	var password = req.body.password;
-// 	var password2 = req.body.password2;
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
 
-// 	// Validation
-// 	req.checkBody('name', 'Name is required').notEmpty();
-// 	req.checkBody('email', 'Email is required').notEmpty();
-// 	req.checkBody('email', 'Email is not valid').isEmail();
-// 	req.checkBody('username', 'Username is required').notEmpty();
-// 	req.checkBody('password', 'Password is required').notEmpty();
-// 	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+	// Validation
+	req.checkBody('name', 'Name cannot be empty').notEmpty();
+	req.checkBody('email', 'Email cannot be empty').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username cannot be empty').notEmpty();
+	req.checkBody('password', 'Password cannot be empty').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
-// 	var errors = req.validationErrors();
+	var errors = req.validationErrors();
 
-// 	if(errors){
-// 		res.render('register',{
-// 			errors:errors
-// 		});
-// 	} else {
-// 		var newUser = new User({
-// 			name: name,
-// 			email:email,
-// 			username: username,
-// 			password: password
-// 		});
+	if(errors){
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		var newUser = new User({
+			name: name,
+			email:email,
+			username: username,
+			password: password
+		});
 
-// 		User.createUser(newUser, function(err, user){
-// 			if(err) throw err;
-// 			console.log(user);
-// 		});
+		User.createUser(newUser, function(err, user){
+			if(err) throw err;
+			console.log(user);
+		});
 
-// 		req.flash('success_msg', 'You are registered and can now login');
+		req.flash('success_msg', 'You are registered and can now login');
 
-// 		res.redirect('/users/login');
-// 	}
-// });
+		res.redirect('/users/login');
+	}
+});
 
 // passport.use(new LocalStrategy(
 //   function(username, password, done) {
