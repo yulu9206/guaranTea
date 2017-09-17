63fd39609fc3e576a5259962fccd6d7896e8c47f
elsieleung59 <32044587+elsieleung59@users.noreply.github.com>
2017-09-17 16:55:25 -0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
+    var x = document.forms["register"]["name"].value;
     if (x == "") {
         alert("Name must be filled out");
         return false;
