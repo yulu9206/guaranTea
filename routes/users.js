7ed3b941628958444cb150c3acfe443a8f703349
yulu9206@gmail.com <yulu9206@Lus-MacBook-Air.local>
2017-11-03 15:04:23 -0700

    minor revision

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
 var User = require('../models/user');
 
 // Register
